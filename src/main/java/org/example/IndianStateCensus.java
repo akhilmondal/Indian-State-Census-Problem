4a205f0f30c311da5a7ce550b0f97a0e953c828a
akhilmondal <akhilmandalfamily@gmail.com>
2023-05-01 12:26:36 +0530

    Added: Readme.md file to the master.

---
+++ src/main/java/org/example/IndianStateCensus.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class IndianStateCensus {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian State Census Program");
+    }
+}
